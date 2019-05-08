b4d2f92323faef860fe6e24338f58ef767c0e7a7
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-07 23:08:36 -0600

    Created a url variable and put a COrs anywhere in the url to prevent CORS error.  Also made a component did mount that fetches the beers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const url = "https://cors-anywhere.herokuapp.com/https://beer.fluentcloud.com/v1/beer"
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
-
+      beers: ""
     }
+  }
+
+  async componentDidMount() {
+    const response = await fetch(url)
+    const json = await response.json()
+    console.log(json)
+    this.setState({ beers: json })
   }
 
   render() {
 
     return (
       <div className="container-fluid">
-      <p>tester</p>
+    
       </div>
     );
   }
